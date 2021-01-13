322501705b667050800fa5ad068fe51b587beeee
Sam Cutler <sam.cutler@gmail.com>
2021-01-13 10:23:59 -0800

    Updated server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
-const app = require('./app');
-const { PORT } = require('./config');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+    res.json( 
+        {
+            ok: true,
+            something: 'worked'
+        } 
+    );
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 })
+
+module.exports = {app}
