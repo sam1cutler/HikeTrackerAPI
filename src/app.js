2f3d159fcc489513701610c8d3ef9edd890c7ef7
Sam Cutler <sam.cutler@gmail.com>
2021-01-23 17:22:02 -0800

    Restored client-specific CORS

--- src/app.js
+++ src/app.js
@@ -18,15 +18,15 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 
-app.use(cors());
+//app.use(cors());
 
-/*
+
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
 );
-*/
+
 
 app.get('/', (req, res) => {
     res.send('Hello, hike-tracker-api user!');
