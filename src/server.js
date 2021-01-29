cc4ab0369eba11a48f9d0a3290a6d83498a9a339
Sam Cutler <sam.cutler@gmail.com>
2021-01-29 10:43:25 -0800

    back to current pg and tweaked pg call in serverjs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config');
+const pg = require('pg');
+pg.defaults.ssl = true;
 
 const db = knex({
     client: 'pg',
