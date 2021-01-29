71daefe5d1d7359b9760d07d9ded5167878203a5
Sam Cutler <sam.cutler@gmail.com>
2021-01-29 10:10:26 -0800

    added error console log

--- src/app.js
+++ src/app.js
@@ -38,6 +38,7 @@ app.use('/api/auth', authRouter);
 
 app.use((error, req, res, next) => {
     let response
+    console.log(error)
     if (NODE_ENV === 'production') {
       response = { error: { message: 'server error' }}
     } else {
